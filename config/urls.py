3e2d88dfc6c126b77bc3e035433c5bdaec717620
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-08-18 04:11:32 +0300

    urlpatterns rework

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
     path('doc/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('doc/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
-
 ]
