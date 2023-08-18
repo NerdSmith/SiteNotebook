1ba5e5dbf159d3130d40f25e05e3b38c9c081a47
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-08-18 04:36:10 +0300

    jwt added logout (blacklist)

--- user_auth/urls.py
+++ user_auth/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
+
 
 jwt_urlpatterns = [
     path('jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('jwt/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
 
 urlpatterns = [
