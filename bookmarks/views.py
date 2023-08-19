50031f0762a6d16076346ac805d08061127d6e64
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-08-19 17:26:08 +0300

    bookmark viewset update

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,8 +1,10 @@
 from django.db import transaction
+from django.db.models import Q
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets, mixins, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from bookmarks.business.OPGInfoExtractor import OPGInfoExtractor
 from bookmarks.models import Bookmark, LinkType
@@ -10,8 +12,8 @@ from bookmarks.permissions import IsOwnerOrAdmin
 from bookmarks.serializers import BookmarkSerializer, BookmarkUrlSerializer
 
 
-class BookmarkViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
-    serializer_class = BookmarkUrlSerializer
+class BookmarkViewSet(ModelViewSet):
+    serializer_class = BookmarkSerializer
     queryset = Bookmark.objects.all()
     permission_classes = [IsOwnerOrAdmin]
 
@@ -41,13 +43,30 @@ class BookmarkViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewset
                 "link_type": link_type.first().pk
             }
 
-        bookmark_serializer = BookmarkSerializer(data=model_data)
+        user_same_url = self.get_queryset().filter(Q(owner=user.pk) & Q(link=info.get("url")))
+        inst = None
+        if user_same_url.exists():
+            inst = user_same_url.first()
+
+        bookmark_serializer = BookmarkSerializer(instance=inst, data=model_data, partial=inst is not None)
         bookmark_serializer.is_valid(raise_exception=True)
         bookmark_serializer.save()
         headers = self.get_success_headers(bookmark_serializer.data)
         return Response(bookmark_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
+    def get_queryset(self):
+        user = self.request.user
+        queryset = super().get_queryset()
+        if self.action == "list" and not user.is_staff:
+            queryset = queryset.filter(owner=user)
+        return queryset
+
     def get_permissions(self):
         if self.action == "create":
             self.permission_classes = [IsAuthenticated, ]
         return super().get_permissions()
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            self.serializer_class = BookmarkUrlSerializer
+        return super().get_serializer_class()
